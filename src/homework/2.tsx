b0930952ef327d055c4cefd202f0db659c543a89
YuriyBeregovoy <rik0681@gmail.com>
2023-10-18 23:19:03 +0300

    Update 2.tsx

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -12,6 +12,12 @@ const initialState: State = {
   requestStep: 'idle',
 };
 
+type Action =
+  | { type: "START_REQUEST" }
+  | { type: "PENDING_REQUEST" }
+  | { type: "FINISH_REQUEST" }
+  | { type: "RESET_REQUEST" };
+
 function requestReducer(state: State, action: Action): State {
   switch (action.type) {
     case 'START_REQUEST':
