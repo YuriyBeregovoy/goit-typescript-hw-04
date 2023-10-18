03fea82c56f1fc95d79c7f383a6ae8949cc82bd1
YuriyBeregovoy <rik0681@gmail.com>
2023-10-18 23:18:32 +0300

    Update 2.tsx

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,4 +1,11 @@
-import React, {useReducer} from "react";
+import React, { useReducer } from "react";
+
+type RequestStep = "idle" | "start" | "pending" | "finished";
+
+type State = {
+  isRequestInProgress: boolean;
+  requestStep: RequestStep;
+};
 
 const initialState: State = {
   isRequestInProgress: false,
